dd0bef58545d309434152bacca403e62f802fcde
Shu <huy.tq204757@sis.hust.edu.vn>
2023-04-28 10:49:18 +0700

    ORA2

--- js/StudentInformation.js
+++ js/StudentInformation.js
@@ -65,17 +65,47 @@ class TTSV {
   getLevel() {
     return this._level;
   }
+  getLevelVal() {
+    if (this._level == "Bậc đào tạo") return "0";
+    if (this._level == "Đại học đại trà") return "1";
+    if (this._level == "Chương trình chất lượng cao") return "2";
+    if (this._level == "Khác") return "3";
+  }
   getMajor() {
     return this._major;
+  }
+  getMajorVal() {
+    if (this._major == "Chương trình đào tạo") return "0";
+    if (this._major == "Khoa học máy tính 2020") return "1";
+    if (this._major == "Kỹ thuật máy tính 2020") return "2";
+    if (this._major == "Khoa/Trung tâm 2020") return "3";
   }
   getSchool() {
     return this._school;
   }
+  getSchoolVal() {
+    if (this._school == "Khoa/Viện quản lý") return "0";
+    if (this._school == "Trường Công nghệ Thông tin va Truyền thông")
+      return "1";
+    if (this._school == "Khác") return "2";
+  }
   getStatus() {
     return this._status;
   }
+  getStatusVal() {
+    if (this._status == "Tình trạng học tập") return "0";
+    if (this._status == "Học") return "1";
+    if (this._status == "Đã ra trường") return "2";
+    if (this._status == "Khác") return "3";
+  }
   getGender() {
     return this._gender;
+  }
+  getGenderVal() {
+    if (this._gender == "Giới tính") return "0";
+    if (this._gender == "Nam") return "1";
+    if (this._gender == "Nữ") return "2";
+    if (this._gender == "Khác") return "3";
   }
   getClass() {
     return this._class;
@@ -83,6 +113,7 @@ class TTSV {
   getCourse() {
     return this._course;
   }
+  getCourseVal() {}
   getEmail() {
     return this._mail;
   }
@@ -90,10 +121,10 @@ class TTSV {
 var ttsv = new TTSV(
   "Tran Quang Huy",
   2020,
-  "Dai hoc dai tra",
-  "Ky thuat may tinh 2020",
-  "Truong Cong nghe thong tin va Truyen thong",
-  "Hoc",
+  "Đại học đại trà",
+  "Kỹ thuật máy tính 2020",
+  "Trường Công nghệ Thông tin va Truyền thông",
+  "Học",
   "Nam",
   "Ky thuat may tinh 02 - K65",
   65,
